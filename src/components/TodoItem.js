4bbd36fac9c55b74eaac6d15dd6c0021767b594d
Tamunokuro <tamunokrobluejack@gmail.com>
2022-12-05 14:54:36 +0100

    Add completed and delete todo logic

---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class TodoItem extends React.Component{
+    render() {
+        return(
+            <React.Fragment>
+            <li>
+            <input type="checkbox" checked={this.props.todo.completed} 
+            onChange={() => this.props.handleChangeProps(this.props.todo.id)}/> {this.props.todo.title}</li> 
+            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default TodoItem
